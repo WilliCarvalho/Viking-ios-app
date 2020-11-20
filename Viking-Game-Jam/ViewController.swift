e6325370502273ffc62d0cb3d21b55fa003a915c
Usuário Convidado <Guest@L2602MICRO33.local>
2020-11-20 16:52:14 -0300

    MapKitView added

--- Viking-Game-Jam/ViewController.swift
+++ Viking-Game-Jam/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
      
+    //63.976092901457235, -22.528768557705853
     
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let annontation = MKPointAnnotation()
+        annontation.coordinate = CLLocationCoordinate2D(latitude: 63.976092901457235, longitude: -22.528768557705853)
+        annontation.title = "Viking World Museum"
+        annontation.subtitle = "Exhibits Viking history and culture"
+        mapView.addAnnotation(annontation)
+        
+        let region = MKCoordinateRegion(center: annontation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: true)
     }
 
     @IBAction func Btn_URL(_ sender: Any) {
